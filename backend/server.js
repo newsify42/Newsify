ff4d3d31099b2884fdb909dafc1a6ea8a4a2bca5
Aaron Boyle <aaronboyle1@gmail.com>
2020-06-09 16:32:32 +0100

    Added a users router to find and add new users

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.ATLAS_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true
+});
 
 mongoose.connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
-})
+});
+
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter);
 
 app.listen(port, ()=> {
     console.log(`Server is running on port: ${port}`);
