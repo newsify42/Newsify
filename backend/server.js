2373a76050e6cb4a8f3274f000457e160c55fb9a
Aaron Boyle <aaronboyle1@gmail.com>
2020-06-09 14:32:45 +0100

    Established a connection to the MongoDB database reading from a local .env file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -8,6 +9,12 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.ATLAS_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
 
 app.listen(port, ()=> {
     console.log(`Server is running on port: ${port}`);
