215efe1ff3fb0c12d087e0018969a3c1618a409d
Aaron Boyle <aaronboyle1@gmail.com>
2020-06-24 17:24:02 +0100

    Cleaned up the server.js file and added comments

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
 require('dotenv').config();
+
+const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware stack
 app.use(cors());
 app.use(express.json());
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
+// Establish a connection to the MongoDB
 mongoose.connect(process.env.ATLAS_URI, {
+    // These options remove deprecation warnings in the MongoDB Node.js driver
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useCreateIndex: true
@@ -19,12 +26,6 @@ mongoose.connect(process.env.ATLAS_URI, {
 mongoose.connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 });
-
-const usersRouter = require('./routes/users');
-const postsRouter = require('./routes/posts');
-
-app.use('/users', usersRouter);
-app.use('/posts', postsRouter);
 
 app.listen(port, ()=> {
     console.log(`Server is running on port: ${port}`);
