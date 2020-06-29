50610f5c4aa3c69e54ab5808dbafd8f5eb0ddb7f
Darragh Ferry <darraghferry@hotmail.com>
2020-06-29 15:17:40 +0100

    added axios helper functions

--- frontend/src/router/RoutesConfig.js
+++ frontend/src/router/RoutesConfig.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 
+import Home from "../pages/Home/Home";
+
 function createPrivateRoute(Component) {
   //logic for creating a private route goes here
   return;
 }
 
 const RoutesConfig = [
-  //routes go here
+  {
+    path: "/",
+    component: Home
+  }
 ];
 
 export default RoutesConfig;
