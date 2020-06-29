c7e43e5ea047a5caebcb1fe54052dc211d23e321
Aaron Boyle <aaronboyle1@gmail.com>
2020-06-29 16:47:48 +0100

    Endpoints for login, register, logout and deleteUser (aren't fully finished). Also added files to create and verify jwts and to validate user input.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,19 +1,125 @@
 const router = require('express').Router();
-let User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-router.route('/').get((req, res) => {
-    User.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json('Error: ' + err));
+const User = require('../models/user.model');
+
+const validate = require('../utils/validate');
+
+router.route('/register').post((req, res) => {
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if(!user) {
+                // Generates the salt and hash for the password
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(req.body.password, salt, (err, hash) => {
+                        const newUser = new User({
+                            email: req.body.email,
+                            password: hash
+                        });
+
+                        // Creates the new user
+                        newUser.save()
+                            .then(user => res.status(201).json({
+                                status: "success",
+                                body: "New account created"
+                            }))
+                            .catch(err => res.status(500).json(err));
+                    });
+                });
+            } else {
+                return res.status(409).json({
+                    status: "info",
+                    body: "Email already taken"
+                });
+            }
+        });
 });
 
-router.route('/add').post((req, res) => {
-    const username = req.body.username;
-    const newUser = new User({username});
+router.route('/login').post((req, res) => {
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if(user) {
+                // Checks if the hashes of the passwords match
+                // Generates and sends a JWT if they match
+                bcrypt.compare(req.body.password, user.password)
+                    .then(isMatch => {
+                        if(isMatch) {
+                            // Token currently expires after 3 hours
+                            const token = jwt.sign({email: req.body.email},
+                                process.env.TOKEN_SECRET, {expiresIn: '120'});
 
-    newUser.save()
-        .then(() => res.json('User added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+                            // Store the JWT in a cookie
+                            res.cookie('Authorization', 'Bearer ' + token);
+                            res.status(200).json({
+                                status: "success",
+                                body: "User logged in",
+                            });
+                        } else {
+                            return res.status(401).json({
+                                status: "error",
+                                body: "Password is incorrect"
+                            });
+                        }
+                    })
+            } else {
+                return res.status(404).json({
+                    status: "error",
+                    body: "Email not found"
+                });
+            }
+        });
+});
+
+router.route('/logout').post((req, res) => {
+    User.findByIdAndUpdate( {
+
+    });
+});
+
+router.route('/updateEmail').put((req, res) => {
+
+});
+
+router.route('/updatePassword').put((req, res) => {
+
+});
+
+router.route('/deleteUser').delete((req, res) => {
+    let token = undefined;
+
+    if(!req.cookies.Authorization) {
+        return res.status(401).json({
+            status: "error",
+            body: "Token not found"
+        });
+    } else {
+        token = req.cookies.Authorization.split(' ')[1];
+    }
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+        console.log(decoded);
+        if(decoded) {
+            bcrypt.compare(req.body.password, user.password)
+                .then(isMatch => {
+                    console.log(isMatch);
+                    if(isMatch) {
+                        return;
+                    } else {
+                        return res.status(401).json({
+                            status: "error",
+                            body: "Password is incorrect"
+                        });
+                    }
+                })   
+        } else {
+            return res.status(401).json({
+                status: "error",
+                body: "Token is invalid or has expired"
+            });
+        }
+    });
 });
 
 module.exports = router;
