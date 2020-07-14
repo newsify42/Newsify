93d372a48a00962f17fbbb732d058288f846d063
Darragh Ferry <darraghferry@hotmail.com>
2020-07-14 16:14:29 +0100

    implemented test geolocation button

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Home Page</h1>;
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log("geolocation not supported by this browser");
+    }
+  }
+  function showPosition(position) {
+    console.log(
+      "lat: ",
+      position.coords.latitude,
+      "lon: ",
+      position.coords.longitude
+    );
+  }
+  return (
+    <>
+      <h1>Home Page</h1>
+      <button onClick={getLocation}>get location</button>
+    </>
+  );
 };
 
 export default Home;
