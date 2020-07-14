989c0cb0e942ad56bae11ea73cad0a448a80fa0d
Darragh Ferry <darraghferry@hotmail.com>
2020-07-14 15:51:54 +0100

    simple check of decoded token to prevent errors

--- frontend/src/router/RoutesConfig.js
+++ frontend/src/router/RoutesConfig.js
@@ -4,9 +4,28 @@ import { Redirect } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Register from "../pages/Register/Register";
 import Login from "../pages/Login/Login";
+import decode from "jwt-decode";
+
+function checkToken() {
+  try {
+    let token = localStorage.getItem("token");
+    const decoded = decode(token);
+    if (
+      decoded.hasOwnProperty("email") &&
+      decoded.hasOwnProperty("exp") &&
+      decoded.hasOwnProperty("iat") &&
+      decoded.hasOwnProperty("id")
+    ) {
+      return true;
+    }
+  } catch (err) {
+    localStorage.clear();
+    return false;
+  }
+}
 
 function createPrivateRoute(Component) {
-  return localStorage.getItem("token") ? (
+  return localStorage.getItem("token") && checkToken() ? (
     <Component />
   ) : (
     <Redirect to="/login" />
