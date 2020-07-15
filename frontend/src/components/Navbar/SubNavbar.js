7956e514ed1413cc933913089b268ad11987b806
Darragh Ferry <darraghferry@hotmail.com>
2020-07-15 14:57:05 +0100

    added placeholder function for SubNav

--- frontend/src/components/Navbar/SubNavbar.js
+++ frontend/src/components/Navbar/SubNavbar.js
@@ -4,6 +4,10 @@ import styles from "./Navbar.module.css";
 import { Layout, Menu, Icon, Button } from "antd";
 
 const SubNav = props => {
+  const getContent = () => {
+    //placeholder function that will get content based on user's perference
+    return;
+  };
   return (
     <Menu
       theme="light"
@@ -11,16 +15,32 @@ const SubNav = props => {
       className={styles.subNav}
       defaultSelectedKeys="1"
     >
-      <Menu.Item key="1" className={styles.menuItem}>
+      <Menu.Item
+        key="1"
+        className={styles.menuItem}
+        onClick={() => getContent()}
+      >
         Global
       </Menu.Item>
-      <Menu.Item key="2" className={styles.menuItem}>
+      <Menu.Item
+        key="2"
+        className={styles.menuItem}
+        onClick={() => getContent()}
+      >
         Spreading
       </Menu.Item>
-      <Menu.Item key="3" className={styles.menuItem}>
+      <Menu.Item
+        key="3"
+        className={styles.menuItem}
+        onClick={() => getContent()}
+      >
         Local
       </Menu.Item>
-      <Menu.Item key="4" className={styles.menuItem}>
+      <Menu.Item
+        key="4"
+        className={styles.menuItem}
+        onClick={() => getContent()}
+      >
         Promoted
       </Menu.Item>
     </Menu>
