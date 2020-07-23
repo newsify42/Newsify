9c208f826a310ed67b510e2daa5898ce48693da0
Darragh Ferry <darraghferry@hotmail.com>
2020-07-23 16:35:40 +0100

    added styles to UpdatePassword form

--- frontend/src/pages/UpdatePassword/UpdatePassword.js
+++ frontend/src/pages/UpdatePassword/UpdatePassword.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Form from "../../components/UpdateForms/UpdatePasswordForm";
 import Nav from "../../components/Navbar/Navbar";
+import styles from "./UpdatePassword.module.css";
 
 export default function UpdatePasswordFrom() {
   return (
     <Nav>
-      <div>
+      <div className={styles.formContainer}>
         <h1>Update Password.</h1>
         <div>
           <Form />
