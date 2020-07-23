5f26f57872898f9007addc12edc6405ebccf80f5
Darragh Ferry <darraghferry@hotmail.com>
2020-07-23 14:28:20 +0100

    wrapped UpdateEmail in Navbar component

--- frontend/src/pages/UpdateEmail/UpdateEmail.js
+++ frontend/src/pages/UpdateEmail/UpdateEmail.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Form from "../../components/UpdateForms/UpdateEmailForm";
+import Nav from "../../components/Navbar/Navbar";
 
 export default function UpdateEmailForm() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.header}>Update Email.</h1>
-      <div className={styles.form_container}>
-        <Form />
+    <Nav>
+      <div className={styles.container}>
+        <h1 className={styles.header}>Update Email.</h1>
+        <div className={styles.form_container}>
+          <Form />
+        </div>
       </div>
-    </div>
+    </Nav>
   );
 }
