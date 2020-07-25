9f5ef4c490943a41ee03809116417f0dc60f6e39
Darragh Ferry <darraghferry@hotmail.com>
2020-07-25 14:37:57 +0100

    moved some code about, added a random-username-genearator module

---
+++ backend/middleware/username.js
@@ -0,0 +1,27 @@
+const asyncHandler = require("express-async-handler");
+require("dotenv").config();
+let rug = require("random-username-generator");
+
+const threadUsername = require("../models/thread_usernames.model");
+
+exports.getUsername = asyncHandler(async (req, res, next) => {
+  let user_id = req.body.user_id;
+  let article_id = req.body.article_id;
+
+  let username = await threadUsername.find({
+    user_id: user_id,
+    article_id: article_id
+  });
+  if (username) {
+    req.body.username = username;
+  } else {
+    let new_username = rug.generate();
+    let username = await threadUsername.save({
+      user_id: user_id,
+      article_id: article_id,
+      username: new_username
+    });
+    req.body.username = username;
+  }
+  next();
+});
