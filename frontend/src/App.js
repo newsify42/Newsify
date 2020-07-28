6736fdfb55bde6d7ab847dae90a5021d467f5a0f
Darragh Ferry <darraghferry@hotmail.com>
2020-07-28 16:25:36 +0100

    removed any code relating to cookies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "antd/dist/antd.css";
 import AppRouter from "./router";
-import { withCookies } from "react-cookie";
 
 const App = props => {
   console.log(props);
   return <AppRouter />;
 };
 
-export default withCookies(App);
+export default App;
